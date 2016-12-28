d8cf44e76786cd0f0b78b662370b0f535f07cc0e
khj <kh@molamil.com>
2016-12-28 17:22:35 +0100

    remove copyright

--- site/web/app/themes/njordsol/functions.php
+++ site/web/app/themes/njordsol/functions.php
@@ -24,3 +24,19 @@ array_map(function ($file) {
         trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
     }
 }, ['helpers', 'setup', 'filters', 'admin']);
+
+
+add_action( 'init', 'custom_remove_footer_credit', 10 );
+function custom_remove_footer_credit () {
+    remove_action( 'storefront_footer', 'storefront_credit', 20 );
+    add_action( 'storefront_footer', 'custom_storefront_credit', 20 );
+}
+
+
+function custom_storefront_credit() {
+    ?>
+    <div class="site-info">
+    &copy; <?php echo get_bloginfo( 'name' ) . ' ' . get_the_date( 'Y' ); ?>
+    </div><!-- .site-info -->
+<?php
+               }
